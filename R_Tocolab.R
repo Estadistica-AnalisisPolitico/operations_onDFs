2881f7bf34afb4c9594b94ec9352c0eb5689e50d
profemagallanes-pucp <jmagallanes@pucp.edu.pe>
2025-03-18 10:02:24 -0700

    Create R_Tocolab.R

---
+++ R_Tocolab.R
@@ -0,0 +1,5 @@
+
+rmarkdown::convert_ipynb('Reshaping_R.ipynb', output = 'Reshaping_R.Rmd')
+rmarkdown::convert_ipynb('aggregating_R.ipynb', output = 'aggregating_R.Rmd')
+                             
+### sos convert index.Rmd index.ipynb #terminal only
